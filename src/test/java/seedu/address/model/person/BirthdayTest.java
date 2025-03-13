7eae7a84ea115723434d778215307212ad4eb17a
FabianHeng <fabianheng@gmail.com>
2025-03-13 13:24:45 +0800

    Create test cases for Birthday field

--- src/test/java/seedu/address/model/person/BirthdayTest.java
+++ src/test/java/seedu/address/model/person/BirthdayTest.java
@@ -1,4 +1,55 @@
 package seedu.address.model.person;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
 public class BirthdayTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Birthday(null));
+    }
+
+    @Test
+    public void constructor_invalidBirthday_throwsIllegalArgumentException() {
+        String invalidBirthday = "9210ds";
+        assertThrows(IllegalArgumentException.class, () -> new Birthday(invalidBirthday));
+    }
+
+    @Test
+    public void isValidBirthday() {
+        // null birthday
+        assertThrows(NullPointerException.class, () -> Birthday.isValidBirthday(null));
+
+        // invalid addresses
+        assertFalse(Birthday.isValidBirthday("")); // empty string
+        assertFalse(Birthday.isValidBirthday(" ")); // spaces only
+
+        // valid addresses
+        assertTrue(Birthday.isValidBirthday("2001-08-12"));
+        assertTrue(Birthday.isValidBirthday("2010-12-12"));
+        assertTrue(Birthday.isValidBirthday("2023-08-27"));
+    }
+
+    @Test
+    public void equals() {
+        Birthday birthday = new Birthday("2001-01-01");
+
+        // same values -> returns true
+        assertTrue(birthday.equals(new Birthday("2001-01-01")));
+
+        // same object -> returns true
+        assertTrue(birthday.equals(birthday));
+
+        // null -> returns false
+        assertFalse(birthday.equals(null));
+
+        // different types -> returns false
+        assertFalse(birthday.equals(5.0f));
+
+        // different values -> returns false
+        assertFalse(birthday.equals(new Birthday("2001-02-02")));
+    }
 }
