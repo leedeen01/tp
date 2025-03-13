aa1028ba723c52080ab833b5ce4aefe7d5913d79
FabianHeng <fabianheng@gmail.com>
2025-03-13 12:40:58 +0800

    Fix test issues

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -28,16 +28,17 @@ public class AddCommand extends Command {
             + PREFIX_PHONE + "PHONE "
             + PREFIX_EMAIL + "EMAIL "
             + PREFIX_ADDRESS + "ADDRESS "
+            + PREFIX_BIRTHDAY + "BIRTHDAY "
+            + PREFIX_PREMIUM + "PREMIUM_NAME PREMIUM_AMOUNT "
             + "[" + PREFIX_TAG + "TAG]...\n"
-            + PREFIX_PREMIUM + "PREMIUM_NAME PREMIUM_AMOUNT\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_BIRTHDAY + "2002-11-24 "
-            + PREFIX_TAG + "friend "
-            + PREFIX_PREMIUM + "vivo360 $1000";
+            + PREFIX_PREMIUM + "vivo360 $1000 "
+            + PREFIX_TAG + "friend ";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
