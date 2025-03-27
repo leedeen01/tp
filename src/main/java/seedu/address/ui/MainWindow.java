5371793faadfe07a5114f85864f4b87cb90f2595
FabianHeng <40349621+FabianHeng@users.noreply.github.com>
2025-03-27 16:03:52 +0800

    Remove trailing whitespace from MainWindow.java

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -139,7 +139,7 @@ public class MainWindow extends UiPart<Stage> {
 
         CommandBox commandBox = new CommandBox(this::executeCommand);
         commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
-      
+
         updateUpcomingBirthdaysPanel();
     }
 
