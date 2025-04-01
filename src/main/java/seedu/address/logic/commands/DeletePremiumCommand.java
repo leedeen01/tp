4ec7ea6aafd50de24889d660f6a0f0f632843b85
NightlyAffair <nithvinlee@gmail.com>
2025-04-01 12:35:01 +0800

    Add javadocs to deleteprcomamnds

--- src/main/java/seedu/address/logic/commands/DeletePremiumCommand.java
+++ src/main/java/seedu/address/logic/commands/DeletePremiumCommand.java
@@ -12,6 +12,10 @@ import java.util.List;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Deletes all premiums from a person identified by the index number in the displayed person list.
+ * This command completely removes any premium information associated with the selected person.
+ */
 public class DeletePremiumCommand extends Command {
     public static final String COMMAND_WORD = "deletepr";
 
@@ -24,10 +28,22 @@ public class DeletePremiumCommand extends Command {
 
     private final Index targetIndex;
 
+    /**
+     * Creates a DeletePremiumCommand to delete premiums from the specified {@code Person}.
+     *
+     * @param targetIndex the index of the person in the filtered person list to delete premiums from
+     */
     public DeletePremiumCommand(Index targetIndex) {
         this.targetIndex = targetIndex;
     }
 
+    /**
+     * Executes the command to delete all premiums from the person at the specified index.
+     *
+     * @param model the model containing the person data
+     * @return a CommandResult indicating the outcome of the premium deletion operation
+     * @throws CommandException if the index is invalid or the person cannot be found
+     */
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
@@ -47,10 +63,25 @@ public class DeletePremiumCommand extends Command {
         return new CommandResult(String.format(MESSAGE_DELETE_PREMIUM_SUCCESS, Messages.formatPremium(premiumDeletedPerson)));
     }
 
+    /**
+     * Creates and returns a {@code Person} with all the same attributes as {@code premiumToDelete}
+     * except with an empty premium list.
+     *
+     * @param premiumToDelete the person whose premiums are to be deleted
+     * @return a new Person with an empty premium list
+     */
     private static Person deletePremium(Person premiumToDelete) {
-        return new Person(premiumToDelete.getName(), premiumToDelete.getPhone(), premiumToDelete.getEmail(), premiumToDelete.getAddress(), premiumToDelete.getBirthday(), new PremiumList(), premiumToDelete.getTags());
+        return new Person(premiumToDelete.getName(), premiumToDelete.getPhone(), premiumToDelete.getEmail(),
+                premiumToDelete.getAddress(), premiumToDelete.getBirthday(), new PremiumList(),
+                premiumToDelete.getTags());
     }
 
+    /**
+     * Compares this DeletePremiumCommand with another object for equality.
+     *
+     * @param other the object to compare with
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object other) {
         if (other == this) {
@@ -65,7 +96,12 @@ public class DeletePremiumCommand extends Command {
         DeletePremiumCommand otherDeletePremiumCommand = (DeletePremiumCommand) other;
         return targetIndex.equals(otherDeletePremiumCommand.targetIndex);
     }
-        
+
+    /**
+     * Returns a string representation of this DeletePremiumCommand.
+     *
+     * @return a string representation of this command
+     */
     @Override
     public String toString() {
         return new ToStringBuilder(this)
--- src/main/java/seedu/address/logic/parser/DeletePremiumCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeletePremiumCommandParser.java
@@ -7,8 +7,20 @@ import seedu.address.logic.parser.exceptions.ParseException;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
+/**
+ * Parses input arguments and creates a new DeletePremiumCommand object.
+ * Accepts an index number representing a person whose premiums will be deleted.
+ */
 public class DeletePremiumCommandParser implements Parser<DeletePremiumCommand> {
 
+    /**
+     * Parses the given {@code String} of arguments in the context of the DeletePremiumCommand
+     * and returns a DeletePremiumCommand object for execution.
+     *
+     * @param args the arguments to parse, expected to be a trimmed index number
+     * @return the DeletePremiumCommand object for execution
+     * @throws ParseException if the user input does not conform to the expected format
+     */
     public DeletePremiumCommand parse(String args) throws ParseException {
         try {
             Index index = ParserUtil.parseIndex(args);
